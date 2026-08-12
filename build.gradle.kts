19931a6d661a47eee2e02457938f46c15e90ed4a
wenqv4617-art <wenqv4617@gmail.com>
2026-08-12 22:50:18 +0800

    222

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,5 +13,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        // ★ nodejs-mobile（内置 Node 运行时）通过 JitPack 分发，必须加入仓库列表
+        maven { url = uri("https://www.jitpack.io") }
     }
 }
