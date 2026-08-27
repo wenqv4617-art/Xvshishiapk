b0850aff716cc0f164b0c4c5f37b85bc1dd61e3d
wenqv4617-art <wenqv4617@gmail.com>
2026-08-27 18:09:09 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,4 @@ allprojects {
         google()
         mavenCentral()
     }
-} // ★ nodejs-mobile（内置 Node 运行时）通过 JitPack 分发，必须加入仓库列表
-        maven { url = uri("https://www.jitpack.io") }
-    }
 }
